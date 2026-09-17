fee5b21634e22216ad30ef2075329c335e03f1af
Nonthawat Pathomchaikoop <pathomchaikoop_n@su.ac.th>
2026-09-17 05:17:14 +0000

    week7_2

--- my-app-week6-movie/src/Component/Navbar.jsx
+++ my-app-week6-movie/src/Component/Navbar.jsx
@@ -17,6 +17,9 @@ function Navbar() {
         <NavLink to="/movies" className={linkClass}>หนังทั้งหมด</NavLink>
         <NavLink to="/about" className={linkClass}>เกี่ยวกับเรา</NavLink>
       </div>
+      <button className="rounded-lg bg-cyan-600 px-4 py-2 font-semibold text-white hover:bg-cyan-700 transition">
+        เข้าสุ่ระบบ
+      </button>
     </nav>
   );
 }
